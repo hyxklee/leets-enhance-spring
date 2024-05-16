009e907ef9f61994f9e19d4ce6d05077718321a3
taeseokyang <ts.yang.0123@gmail.com>
2024-05-16 19:00:14 +0900

    init

---
+++ src/test/java/leets/enhance/EnhanceApplicationTests.java
@@ -0,0 +1,13 @@
+package leets.enhance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
