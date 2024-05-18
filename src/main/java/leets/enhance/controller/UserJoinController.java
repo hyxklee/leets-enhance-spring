5d4593c16533829b37361925f5c43d62a6b708c6
hyxklee <ewgt1234@naver.com>
2024-05-18 11:35:46 +0900

    feat: 회원가입 및 중복이메일 확인 컨트롤러 구현

--- src/main/java/leets/enhance/controller/UserJoinController.java
+++ src/main/java/leets/enhance/controller/UserJoinController.java
@@ -1,4 +1,33 @@
 package leets.enhance.controller;
 
+import leets.enhance.dto.UserJoinDTO;
+import leets.enhance.service.UserJoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserJoinController {
+
+    private final UserJoinService userJoinService;
+
+    @PostMapping("/users/register")
+    public ResponseEntity<String> registerUser(UserJoinDTO dto) {
+        userJoinService.join(dto);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @GetMapping("/users/check-duplicate-id")
+    public ResponseEntity<String> checkDuplicateId(UserJoinDTO dto){
+        try {
+            userJoinService.validDuplicate(dto.getEmail());
+        } catch (IllegalStateException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("이미 존재하는 이메일 입니다.");
+        }
+        return ResponseEntity.ok("사용 가능한 이메일 입니다.");
+    }
 }
