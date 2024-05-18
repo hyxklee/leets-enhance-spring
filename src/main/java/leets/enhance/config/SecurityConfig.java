87e62fa26f81a8580c7fbdfcd3b4222350a368be
hyxklee <ewgt1234@naver.com>
2024-05-18 11:31:16 +0900

    feat(SecurityConfig): 프로그램 초기 설정 및 비밀번호 인코딩 빈 등록

--- src/main/java/leets/enhance/config/SecurityConfig.java
+++ src/main/java/leets/enhance/config/SecurityConfig.java
@@ -1,4 +1,40 @@
 package leets.enhance.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests((auth)->auth
+                        .requestMatchers("/users/login", "/users/register",
+                                "/users/check-duplicate-id",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/swagger/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
 }
