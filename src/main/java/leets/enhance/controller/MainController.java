4a5af5ece881a36a158601d3f07fe66931d347c8
hyxklee <ewgt1234@naver.com>
2024-07-03 16:57:28 +0900

    refeat: docker yml로 변경

--- src/main/java/leets/enhance/controller/MainController.java
+++ src/main/java/leets/enhance/controller/MainController.java
@@ -1,7 +1,9 @@
 package leets.enhance.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MainController {
     @GetMapping("/main")
     public String hello(){
